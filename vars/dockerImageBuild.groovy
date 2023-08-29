64928929105ff8399bdf7bfc1560f8fd125e5ad1
satishlakshmiudemy@gmail.com <satishlakshmiudemy@gmail.com>
2023-08-29 14:59:01 +0530

    Docker Image Build

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -2,6 +2,5 @@ def call(String serviceName,String dockerImageTag,String dockerHubUser){
     sh """ 
     docker image build -t ${dockerHubUser}/${serviceName} .
     docker image tag ${dockerHubUser}/${serviceName} ${dockerHubUser}/${serviceName}:${dockerImageTag}
-    docker image tag ${dockerHubUser}/${serviceName} ${dockerHubUser}/${serviceName}:latest
     """
 }
